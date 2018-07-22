ddc1fe503304f89627fc53a73c8f8ed211d81fd3
Pavle Batuta <pavledb@gmail.com>
2018-07-22 16:17:49 +0200

    Add tests for adjustable metadata

--- test/WithAdjustableMetadata.test.js
+++ test/WithAdjustableMetadata.test.js
@@ -7,15 +7,27 @@ require('chai')
     .should();
 
 contract('WithAdjustableMetadata', function([owner, accounts]) {
+
     const NAME = "Adjustable Thing";
+    const NEW_NAME = "Changed name";
     const DESCRIPTION = "Just an adjustable thing";
+    const NEW_DESCRIPTION = "Changed description";
     const META = "<null>";
+    const NEW_META = "Changed meta";
 
     beforeEach(async function(){
         this.metaInstance = await WithAdjustableMetadata.new(NAME, DESCRIPTION, META);        
     });
 
-    it('should allow owner to change the name', async function() {
+    it('should allow the owner to change the name', async function() {
+        this.metaInstance.exist;
+
+        await this.metaInstance.adjustName(NEW_NAME, { from: owner });
+        const ret = await this.metaInstance.name();
+        ret.should.be.equal(NEW_NAME);
+    });
+
+    it('should not allow anyone other than owner to change the name', async function() {
         this.metaInstance.exist;
         const other = accounts[2];
 
@@ -23,24 +35,37 @@ contract('WithAdjustableMetadata', function([owner, accounts]) {
             .should.be.rejectedWith(EVMRevert);
     });
 
-    // it('should return the correct name of the instance', async function() {
-    //     this.metaInstance.should.exist;
+    it('should allow the owner to change the description', async function() {
+        this.metaInstance.exist;
 
-    //     const returnedName = await this.metaInstance.name();
-    //     returnedName.should.be.equal(NAME);
-    // });
+        await this.metaInstance.adjustDescription(NEW_DESCRIPTION, { from: owner });
+        const ret = await this.metaInstance.description();
+        ret.should.be.equal(NEW_DESCRIPTION);
+    });
 
-    // it('should return the correct description of the instance', async function() {
-    //     this.metaInstance.should.exist;
+    it('should not allow anyone other than owner to change the description', async function() {
+        this.metaInstance.exist;
+        const other = accounts[2];
 
-    //     const returnedDescription = await this.metaInstance.description();
-    //     returnedDescription.should.be.equal(DESCRIPTION);
-    // });
+        await this.metaInstance.adjustDescription({ from: other })
+            .should.be.rejectedWith(EVMRevert);
+    });
 
-    // it('should return the correct instance metadata', async function() {
-    //     this.metaInstance.should.exist;
+    it('should allow the owner to change the metadata', async function() {
+        this.metaInstance.exist;
 
-    //     const returnedMetadata = await this.metaInstance.metadata();
-    //     returnedMetadata.should.be.equal(META);
-    // });
+        const newName =
+            await this.metaInstance.adjustMetadata(NEW_META, { from: owner });
+
+        const ret = await this.metaInstance.metadata();
+        ret.should.be.equal(NEW_META);
+    });
+
+    it('should not allow anyone other than owner to change the metadata', async function() {
+        this.metaInstance.exist;
+        const other = accounts[2];
+
+        await this.metaInstance.adjustMetadata({ from: other })
+            .should.be.rejectedWith(EVMRevert);
+    });
 });
