cd2356e11c19a7b1cfa1798a5c8e2ff7deac5cde
Pavle Batuta <pavledb@gmail.com>
2018-07-22 14:05:49 +0200

    Add adjustable metadata contract

---
+++ contracts/WithAdjustableMetadata.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.4.24;
+
+import "./WithMetadata.sol";
+
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract WithAdjustableMetadata is Ownable, WithMetadata {
+
+    event AdjustedName(string name);
+    event AdjustedDescription(string description);
+    event AdjustedMetadata(string metadata);
+
+    constructor(
+        string _name,
+        string _description,
+        string _metadata
+    )
+        public
+        WithMetadata(_name, _description, _metadata)
+    {
+
+    }
+
+
+    function adjustName(string _name) 
+        public
+        onlyOwner
+    {
+        name = _name;
+    }
+
+    function adjustDescription(string _description) 
+        public
+        onlyOwner
+    {
+        description = _description;
+    }
+
+    function adjustMetadata(string _metadata)
+        public
+        onlyOwner
+    {
+        metadata = _metadata;
+    }
+}
