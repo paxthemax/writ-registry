5886b8008759078361ac7c41534e37253c2b043c
Pavle Batuta <pavledb@gmail.com>
2018-07-23 09:05:40 +0200

    Update tests
    Simplified, instatiated at the beginning.

--- test/WithMetadata.test.js
+++ test/WithMetadata.test.js
@@ -9,28 +9,23 @@ contract('WithMetadata', function() {
     const DESCRIPTION = "Just a thing";
     const META = "<null>";
 
-    beforeEach(async function(){
+    before(async function(){
         this.metaInstance = await WithMetadata.new(NAME, DESCRIPTION, META);
-    })
+    });
+
+    it('should be properly initialized', function() {
+        this.metaInstance.should.exist;
+    });
 
     it('should return the correct name of the instance', async function() {
-        this.metaInstance.should.exist;
-
-        const returnedName = await this.metaInstance.name();
-        returnedName.should.be.equal(NAME);
+        (await this.metaInstance.name()).should.be.equal(NAME);
     });
 
     it('should return the correct description of the instance', async function() {
-        this.metaInstance.should.exist;
-
-        const returnedDescription = await this.metaInstance.description();
-        returnedDescription.should.be.equal(DESCRIPTION);
+        (await this.metaInstance.description()).should.be.equal(DESCRIPTION);
     });
 
     it('should return the correct instance metadata', async function() {
-        this.metaInstance.should.exist;
-
-        const returnedMetadata = await this.metaInstance.metadata();
-        returnedMetadata.should.be.equal(META);
+        (await this.metaInstance.metadata()).should.be.equal(META);
     });
 });
