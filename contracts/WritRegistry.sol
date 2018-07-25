f860a6321512b62b62bb9c7874111c5cc239b1e9
Pavle Batuta <pavledb@gmail.com>
2018-07-25 11:35:18 +0200

    Added create and get writ methods
    * create method creates a new writ contract
    * get method fetches all writ contracts

--- contracts/WritRegistry.sol
+++ contracts/WritRegistry.sol
@@ -1,10 +1,11 @@
 pragma solidity ^0.4.24;
 
 import "./WithAdjustableMetadata.sol";
+import "./Writ.sol";
 
 contract WritRegistry is WithAdjustableMetadata {
-
-    address[] public writContracts;
+    mapping(address => address[]) public ownerToWritContracts;
+    uint256 public writContractCnt;
 
     event WritContractCreated(address _address);
 
@@ -17,6 +18,30 @@ contract WritRegistry is WithAdjustableMetadata {
         WithAdjustableMetadata(_name, _description, _metadata)
     {
 
+    }
+
+    function createWrit(
+        string _name,
+        string _description,
+        string _metadata,
+        string _data
+    )
+        public
+        returns (uint256 index)
+    {
+        address newContract = new Writ(_name, _description, _metadata, _data);
+        writContractCnt++;
+        return ownerToWritContracts[msg.sender].push(newContract) - 1;
+    }
+
+    function getWrits(
+        address _owner
+    )
+        public
+        view
+        returns (address[])
+    {
+        return ownerToWritContracts[_owner];
     }
 
 }
